dc9f78fb370a9df228af97508534494bbf0ec606
Payal Dilip Ashtikar <payalashtikar@gmail.com>
2022-05-28 23:22:42 +0530

    Login and Signup

---
+++ frontend/src/Components/Signup.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login_Signup.css';
+
+const Login = () => {
+    return (
+        <div className="Login">
+            <div className="container">
+                <h1>Logo</h1>
+                <h5>create new account</h5>
+                <div className='wrapper-inp-btn'>
+                    <input type="text" placeholder="User ID" />
+                    <input type="text" placeholder="Password" />
+                    <input type="text" placeholder="Confirmed Password" />
+                    <button type="submit">Sign up</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
