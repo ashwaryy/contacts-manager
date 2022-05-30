8a46c72460e2fc346ee596173023c568e919c236
ashwaryy <ashwaryy@gmail.com>
2022-05-31 02:12:36 +0530

    landing page styling

--- frontend/src/Components/LandingPage.js
+++ frontend/src/Components/LandingPage.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './LandingPage.css';
+import React from "react";
+import "./LandingPage.css";
 const LandingPage = () => {
-    return (
-        <div className='LandingPage'>
-
-            {/* <div className='name'>
+  return (
+    <div className="LandingPage">
+      {/* <div className='name'>
                 <em>Group-3</em>
             </div> */}
-            <div className='Landing'>
-
-                <div className='heading'>
-                    <h1>Contact Manager Project</h1>
-                </div>
-                <div className='btn'>
-
-                    <button type='submit' ><a href="/login">Click to Start..</a></button>
-                </div>
-            </div>
+      <div className="Landing">
+        <div className="heading">
+          <h1>Contacts Manager</h1>
         </div>
-    )
-}
+        <div className="btn">
+          <button type="submit">
+            <a href="/main">Click to Start..</a>
+          </button>
+        </div>
+        <p>
+          Not a user? <a href="/signup">Sign Up</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 export default LandingPage;
