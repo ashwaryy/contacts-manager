cf4c907c48b98a6528abd86ea69914ee18d6f133
Payal Dilip Ashtikar <payalashtikar@gmail.com>
2022-05-30 23:24:32 +0530

    LAndingpage work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ImportCSV from "./components/ImportCSV";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import LandingPage from "./components/LandingPage";
 import DisplayContacts from "./components/DisplayContacts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<LandingPage/>} />
       <Route path="/import" element={<ImportCSV />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
